dff684c98cfad1f37744ad01b2cd882e15413499
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-17 11:47:39 -0700

    Create Pet class in models.py

--- models.py
+++ models.py
@@ -14,4 +14,26 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class Pet(db.Model):
+
+    __tablename__ = 'pets'
+
+    id = db.column(db.Integer,
+        primary_key=True,
+        autoincremement=True)
+    name = db.column(db.String(25),
+        nullable=False)
+    species = db.column(db.String(25),
+        nullable=False)
+    photo_url = db.column(db.Text,
+        nullable=False,
+        default='')
+    age = db.column(db.String(10),
+        db.CheckConstraint("age in ['baby', 'young', 'adult', 'senior']"),
+        nullable=False)
+    notes = db.column(db.Text,
+        nullable=False,
+        default='')
+
+
 
